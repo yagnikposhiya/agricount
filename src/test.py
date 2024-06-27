2d392bf38da6aab13c2d9d5a630f15a8f9ff02eb
yagnikposhiya <yagnikposhiya97275@gmail.com>
2024-06-28 03:27:54 +0530

    added codebase for inference using segment_anything_model

--- src/test.py
+++ src/test.py
@@ -4,68 +4,40 @@ github: @yagnikposhiya
 organization: NEUROWORK Research Labs
 """
 
+import cv2
 import torch
 import torchvision
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
-from PIL import Image
 from config.config import Config
-from torchvision import transforms
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
 
 if __name__ == '__main__':
 
     config = Config() # create an instance of config class
 
-    # load a pre-trained SSD model
-    model = torchvision.models.detection.ssd300_vgg16(pretrained=True)
-    model.eval()
+    image = cv2.imread(config.SAMPLE_IMAGE_PATH) # load an image
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # convert an image from BGR2RGB
 
-    image = Image.open(config.SAMPLE_IMAGE_PATH) # load an image
-    transform = transforms.Compose([
-        transforms.Resize((300,300)),
-        transforms.ToTensor()
-    ]) # transform a loaded image
+    sam_checkpoint = "src/sam_vit_h_4b8939.pth" # set model checkpoint
+    model_type = "vit_h" # set model type
 
-    image = transform(image).unsqueeze(0) # add batch dimension
-
-    # perform inference
-    with torch.no_grad():
-        predictions = model(image)
-
-    # extract predictions
-    print(predictions)
-
-    # Extract predictions
-    predicted_boxes = predictions[0]['boxes'].cpu().numpy()
-    predicted_scores = predictions[0]['scores'].cpu().numpy()
-    predicted_labels = predictions[0]['labels'].cpu().numpy()
-
-    # process input image
-    image = image.squeeze(0) # remove batch dimension
-    print("- Shape of an image: {}".format(image.shape))
-    print("- Type of an image: {}".format(type(image)))
-    numpy_image = image.cpu().detach().numpy() # move tensor to CPU if it is on GPU, detach tensor if it requires gradients, and convert it to numpy array
-    print("- Shape of a numpy image: {}".format(numpy_image.shape))
-    print("- Type of a numpy image: {}".format(type(numpy_image)))
-    numpy_image = np.transpose(numpy_image,(1,2,0)) # change shape of ndarray from (3,300,300) to (300,300,3)
+    device = "cuda" if torch.cuda.is_available() else "cpu" # set available device
     
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint) # load a model from model registry
+    sam.to(device=device) # move model to GPU if it is available
 
-    # Visualize the results
-    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20,20)) # create figure canvas 
-    ax.imshow(numpy_image)
+    mask_generator_ = SamAutomaticMaskGenerator(
+        model=sam,
+        points_per_side=32,
+        pred_iou_thresh=0.9,
+        stability_score_thresh=0.96,
+        crop_n_layers=1,
+        crop_n_points_downscale_factor=2,
+        min_mask_region_area=100, # required opencv to run post-processing
+    )
 
-    # Define a threshold to filter out low-confidence detections
-    confidence_threshold = 0.1
-
-    for box, score, label in zip(predicted_boxes, predicted_scores, predicted_labels):
-        if score > confidence_threshold:
-            # Draw bounding box
-            x_min, y_min, x_max, y_max = box
-            rect = patches.Rectangle((x_min, y_min), x_max - x_min, y_max - y_min, linewidth=1, edgecolor='r', facecolor='none')
-            ax.add_patch(rect)
-            # Draw label and score
-            ax.text(x_min, y_min - 10, f'{label}: {score:.2f}', color='red', fontsize=12, backgroundcolor='none')
-
-    plt.show()
+    masks = mask_generator_.generate(image)
+    print(len(masks))
+    
