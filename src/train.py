ed717caa75f7401755d8ee964b5d8064845adb80
yagnikposhiya <yagnikposhiya97275@gmail.com>
2024-07-25 20:19:34 +0530

    modified training pipeline; modified params list of look_at_json_structure() & createMasks() functions and added merge_json_files() function

--- src/train.py
+++ src/train.py
@@ -4,9 +4,12 @@ github: @yagnikposhiya
 organization: Tvisi
 """
 
+import glob
+
 from config.config import Config
 from gpu_config.check import check_gpu_config
 from customdata import look_at_json_structure, createMasks
+from utils.utils import merge_json_files
 
 
 if __name__ == '__main__':
@@ -20,11 +23,15 @@ if __name__ == '__main__':
     print("--------------UNDERSTANDING JSON FILE STRUCTURE------------")
     print("-----------------------------------------------------------")
 
-    look_at_json_structure(config.INPUT_JSON_FILE_PATH) # understand JSON file structure and which information it contains
+    look_at_json_structure(config.SAMPLE_JSON_FILE_PATH) # understand JSON file structure and which information it contains
 
     print("-----------------------------------------------------------")
     print("------------------CREATING PROCESSED DATASET---------------")
     print("-----------------------------------------------------------")
     
+    input_files = glob.glob(f'{config.INPUT_JSON_FILE_PATH}/File*.json') # create list of json files available @ config.INPUT_JSON_FILE_PATH
+    output_file = f'{config.INPUT_JSON_FILE_PATH}/merge.json' # define path for json file, contains merged data from multiple json files
+    merge_json_files(input_files=input_files, output_file=output_file) # merge multiple json files
+
     print("Creating mask images from json data...")
-    createMasks(config.INPUT_JSON_FILE_PATH, config.RAW_IMAGE_DIR, config.BASE_DATA_PATH) # create mask images from existing mask region information i.e. XY co-ordinates
+    createMasks(output_file, config.RAW_IMAGE_DIR, config.BASE_DATA_PATH) # create mask images from existing mask region information i.e. XY co-ordinates
