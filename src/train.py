cc1797b17e0eb197b97b1f1382a2a93199091599
yagnikposhiya <yagnikposhiya97275@gmail.com>
2024-07-26 12:45:32 +0530

    appeded data loading procedure to the model training pipeline

--- src/train.py
+++ src/train.py
@@ -6,9 +6,11 @@ organization: Tvisi
 
 import os
 import glob
+import torch
 
 from config.config import Config
 from gpu_config.check import check_gpu_config
+from customdata import SegmentationDataModule
 from customdata import look_at_json_structure, createMasks
 from utils.utils import merge_json_files, show_mask_image_pair
 
@@ -41,3 +43,12 @@ if __name__ == '__main__':
 
     show_mask_image_pair(image_dir=os.path.join(config.TRAINSET_PATH,'images'),
                          mask_dir=os.path.join(config.TRAINSET_PATH,'masks')) # visualize mask-image pairs
+    
+    config.TRAIN_IMAGE_DIR = os.path.join(config.TRAINSET_PATH,'images') # set path to a directory contains input images for training
+    config.TRAIN_MASK_DIR = os.path.join(config.TRAINSET_PATH, 'masks') # set path to a directory contains input mask images for training
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # set device for model training
+    data_module = SegmentationDataModule(train_image_dir=config.TRAIN_IMAGE_DIR,
+                                         train_mask_dir=config.TRAIN_MASK_DIR,
+                                         batch_size=config.BATCH_SIZE,
+                                         transform=config.TRANSFORM) # initialize the data module
